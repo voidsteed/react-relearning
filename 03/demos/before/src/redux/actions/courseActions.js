d35dd83b5b9d5b86061678bf0143f8372ecb4319
Yujun Liu <dreamzack@gmail.com>
2019-05-22 00:40:14 -0700

    hook

--- 03/demos/before/src/redux/actions/courseActions.js
+++ 03/demos/before/src/redux/actions/courseActions.js
@@ -1,5 +1,42 @@
 import * as types from "./actionTypes";
+import * as courseApi from "../../api/courseApi";
 
-export const createCourse = course => {
-  return { type: types.CREATE_COURSE, course };
-};
+export function loadCoursesSuccess(courses) {
+  return { type: types.LOAD_COURSES_SUCCESS, courses };
+}
+
+export function loadCourses() {
+  return function(dispatch) {
+    return courseApi
+      .getCourses()
+      .then(courses => {
+        dispatch(loadCoursesSuccess(courses));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+}
+
+export function updateCourseSuccess(course) {
+  return { type: types.UPDATE_COURSE_SUCCESS, course };
+}
+export function createCourseSuccess(course) {
+  return { type: types.CREATE_COURSE_SUCCESS, course };
+}
+
+export function saveCourse(course) {
+  //eslint-disable-next-line no-unused-vars
+  return function(dispatch, getState) {
+    return courseApi
+      .saveCourse(course)
+      .then(savedCourse => {
+        course.id
+          ? dispatch(updateCourseSuccess(savedCourse))
+          : dispatch(createCourseSuccess(savedCourse));
+      })
+      .catch(error => {
+        throw error;
+      });
+  };
+}
